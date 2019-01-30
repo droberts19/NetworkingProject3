a90d4e8d9d0276ee1f204c67eb184f41f0086f47
droberts19 <droberts19@scienceleadership.org>
2019-01-30 08:17:59 -0500

    commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,66 @@
 package sample;
 
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+
 public class Controller {
+
+    public Path path;
+    public Group lineGroup;
+    public Button btnClear;
+    public Rectangle canvas;
+    public TextField guessText;
+    public Button guessButton;
+    public Button send;
+
+    public void init() {
+        lineGroup = new Group();
+
+        canvas.setFill(Color.LIGHTGRAY);
+        canvas.setOnMousePressed(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent me) {
+
+                path = new Path();
+                path.setMouseTransparent(true);
+                path.setStrokeWidth(2.0);
+                path.setStroke(Color.BLACK);
+                lineGroup.getChildren().add(path);
+                path.getElements().add(new MoveTo(me.getSceneX(), me.getSceneY()));
+            }
+        });
+
+        canvas.setOnMouseReleased(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent me) {
+                path = null;
+
+            }
+        });
+
+        canvas.setOnMouseDragged(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent me) {
+
+                // keep lines within rectangle
+
+                if (canvas.getBoundsInLocal().contains(me.getX(), me.getY())) {
+                    path.getElements().add(new LineTo(me.getSceneX(), me.getSceneY()));
+                }
+
+            }
+        });
+    }
+
+    public void clear() {
+        lineGroup.getChildren().removeAll(lineGroup.getChildren());
+    }
 }
