be87c5a8b4ca5908dd3b44f06a346ef64285cd21
ma.valerieberta <vberta20@scienceleadership.org>
2019-02-27 08:32:44 -0500

    done

--- src/sample/GuessMessage.java
+++ src/sample/GuessMessage.java
@@ -1,5 +1,10 @@
 package sample;
 
+import javafx.embed.swing.SwingFXUtils;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 // Serializable means that objects of this class can be read/written over ObjectStreams
@@ -25,4 +30,18 @@ public class GuessMessage implements Serializable {
     public String toString() {
         return "\" from: " + sender + "\" with guess: " + guess;
     }
+
+    private void readObject(ObjectInputStream inStream) throws IOException, ClassNotFoundException {
+        // this reads sender String with default code
+        inStream.defaultReadObject();
+        // this reads data Image using this custom code
+    }
+
+    private void writeObject(ObjectOutputStream outStream) throws IOException {
+        // this writes sender String with default code
+        outStream.defaultWriteObject();
+        // this writes data Image using this custom code
+    }
+
+
 }
